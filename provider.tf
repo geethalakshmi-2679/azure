16b8796dfbced4b189215419784a5182324a7972
geethalakshmi-2679 <geetha.selvamk26@gmail.com>
2025-09-04 15:03:15 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
   features {}
   subscription_id = "79afec09-f1cf-464c-8cb2-0a8a6fd4b2b4" # Azure Cloudrho Lab
-  tenant_id       = "b2d1259c-5c01-459b-a99f-8d3989688f8b" # Correct tenant
+  tenant_id       = "b2d1259c-5c01-459b-a99f-8d3989688f8b" # changeable id
 }
